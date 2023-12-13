6d9168671df3a91a592d24edb790805b6c293176
212410368 <212410368@o365.tku.edu.tw>
2023-12-13 23:54:31 +0800

    W13-P5: Use mega menu theme to show w03-card page

---
+++ demo/script_68.js
@@ -0,0 +1,27 @@
+function showdemo(week) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(week){
+        case 2:
+            p.innerHTML = `<iframe src="./w02_68_into/index.html" width="100%" height="100%" />`;
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./w03_68_card/index.html" width="100%" height="100%" />`;
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./w04_68_blogs/index.html" width="100%" height="100%" />`;
+            break;
+        case 5:
+            p.innerHTML = `<iframe src="./w05_68_blogs/index.html" width="100%" height="100%" />`;
+            break;
+        case 6:
+            p.innerHTML = `<iframe src="./w06_68_navbar/index.html" width="100%" height="100%" />`;
+            break;
+        case 7:
+            p.innerHTML = `<iframe src="./index.html" width="100%" height="100%" />`;
+            break;
+   
+
+
+    }
+}
