fbf226e0d468d299b93e09e21465d5a9d449059d
212410368 <212410368@o365.tku.edu.tw>
2023-12-14 21:19:41 +0800

    W14-P1: Link from homepage to class demo page, and link back to homepage

--- demo/script_68.js
+++ demo/script_68.js
@@ -18,7 +18,7 @@ function showdemo(week) {
             p.innerHTML = `<iframe src="./w06_68_navbar/index.html" width="100%" height="100%" />`;
             break;
         case 7:
-            p.innerHTML = `<iframe src="./index.html" width="100%" height="100%" />`;
+            p.innerHTML = `<iframe src="./w07_68_tile/index.html" width="100%" height="100%" />`;
             break;
    
 
--- js/script_68.js
+++ js/script_68.js
@@ -1,10 +1,27 @@
-const navbar = document.querySelector('.nav');
-console.log('navbar', navbar);
+function showdemo(week) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(week){
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w02_68_intro/index.html" width="100%" height="100%" />`;
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w03_68_card/index.html" width="100%" height="100%" />`;
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./demo/w04_68_blogs/index.html" width="100%" height="100%" />`;
+            break;
+        case 5:
+            p.innerHTML = `<iframe src="./demo/w05_68_blogs/index.html" width="100%" height="100%" />`;
+            break;
+        case 6:
+            p.innerHTML = `<iframe src="./demo/w06_68_navbar/index.html" width="100%" height="100%" />`;
+            break;
+        case 7:
+            p.innerHTML = `<iframe src="./demo/w07_68_tile/index.html" width="100%" height="100%" />`;
+            break;
+   
 
-window.addEventListener('scroll', function () {
-  if(this.window.pageYOffset > 100) {
-    navbar.classList.add('navbar-fixed');
-  } else {
-    navbar.classList.remove('navbar-fixed');
-  }
-});
+
+    }
+}
